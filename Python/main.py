513f77a7235495c1f8a9771064b5595421fe9fae
FixingMind5 <aguilarmanu5@gmail.com>
2019-06-11 19:52:51 -0500

    Initializating some classes in python and Java

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,12 @@
+from car import Car
+
 if __name__ == '__main__':
     print("Hello world!")
+
+    car = Car()
+    car.id = 1234
+    car.license = "AMQ123"
+    car.driver = "Andrés Herrera"
+
+    print("License: {}, Driver: {} ".format(car.license, car.driver))
+    # print(vars(car)) Con esto podemos imprimir todo lo que esté dentro de esa varible
